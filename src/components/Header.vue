a750e2c10c6b4d40138136709816c22aff24cc3f
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-03-30 14:34:47 +0200

    started project with basic installations

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    <h1 class="text-primary">ciao</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/scss/partials/_variables";
+
+</style>
